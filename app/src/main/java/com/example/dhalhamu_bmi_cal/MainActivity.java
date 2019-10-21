e816cc863634525d9e0bb7392a742d5605a6a762
Dha <dawalhamo37@gmail.com>
2019-10-21 07:51:15 +0545

    first commit

---
+++ app/src/main/java/com/example/dhalhamu_bmi_cal/MainActivity.java
@@ -0,0 +1,69 @@
+package com.example.dhalhamu_bmi_cal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.example.dhalhamu_bmi_cal.R;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText height;
+    private EditText weight;
+    private TextView result;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        height = (EditText) findViewById(R.id.height);
+        weight = (EditText) findViewById(R.id.weight);
+        result = (TextView) findViewById(R.id.result);
+    }
+
+    public void calculateBMI(View v) {
+        String heightStr = height.getText().toString();
+        String weightStr = weight.getText().toString();
+
+        if (heightStr != null && !"".equals(heightStr)
+                && weightStr != null && !"".equals(weightStr)) {
+            float heightValue = Float.parseFloat(heightStr) / 100;
+            float weightValue = Float.parseFloat(weightStr);
+
+            float bmi = weightValue / (heightValue * heightValue);
+
+            displayBMI(bmi);
+        }
+    }
+    private void displayBMI(float bmi) {
+        String bmiLabel = "";
+
+        if (Float.compare(bmi, 15f) <= 0) {
+            bmiLabel = getString(R.string.app_name);
+        } else if (Float.compare(bmi, 15f) > 0  &&  Float.compare(bmi, 16f) <= 0) {
+            bmiLabel = getString(R.string.app_name);
+        } else if (Float.compare(bmi, 16f) > 0  &&  Float.compare(bmi, 18.5f) <= 0) {
+            bmiLabel = getString(R.string.app_name);
+        } else if (Float.compare(bmi, 18.5f) > 0  &&  Float.compare(bmi, 25f) <= 0) {
+            bmiLabel = getString(R.string.app_name);
+        } else if (Float.compare(bmi, 25f) > 0  &&  Float.compare(bmi, 30f) <= 0) {
+            bmiLabel = getString(R.string.app_name);
+        } else if (Float.compare(bmi, 30f) > 0  &&  Float.compare(bmi, 35f) <= 0) {
+            bmiLabel = getString(R.string.app_name);
+        } else if (Float.compare(bmi, 35f) > 0  &&  Float.compare(bmi, 40f) <= 0) {
+            bmiLabel = getString(R.string.app_name);
+        } else {
+            bmiLabel = getString(R.string.app_name);
+        }
+
+        bmiLabel = bmi + "\n\n" + bmiLabel;
+        result.setText(bmiLabel);
+    }
+}
+
+
+
+
+
