f2bcb361ba4ca4f532f4293b2fd76c4773f33448
Dha <dawalhamo37@gmail.com>
2019-10-21 12:02:26 +0545

    add screenshot

--- app/src/main/java/com/example/dhalhamu_bmi_cal/MainActivity.java
+++ app/src/main/java/com/example/dhalhamu_bmi_cal/MainActivity.java
@@ -3,67 +3,68 @@ package com.example.dhalhamu_bmi_cal;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
 import android.view.View;
+
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
-import com.example.dhalhamu_bmi_cal.R;
+import java.text.DecimalFormat;
 
-public class MainActivity extends AppCompatActivity {
-    private EditText height;
-    private EditText weight;
-    private TextView result;
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    private EditText etHeight, etWeight;
+    private TextView tvResult;
+    private Button btnCalculate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        height = (EditText) findViewById(R.id.height);
-        weight = (EditText) findViewById(R.id.weight);
-        result = (TextView) findViewById(R.id.result);
+
+        initView();
+        btnCalculate.setOnClickListener(this);
     }
 
-    public void calculateBMI(View v) {
-        String heightStr = height.getText().toString();
-        String weightStr = weight.getText().toString();
-
-        if (heightStr != null && !"".equals(heightStr)
-                && weightStr != null && !"".equals(weightStr)) {
-            float heightValue = Float.parseFloat(heightStr) / 100;
-            float weightValue = Float.parseFloat(weightStr);
-
-            float bmi = weightValue / (heightValue * heightValue);
-
-            displayBMI(bmi);
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.calc) {
+            if (isValid())
+                showResult();
+            else
+                showError();
         }
     }
-    private void displayBMI(float bmi) {
-        String bmiLabel = "";
 
-        if (Float.compare(bmi, 15f) <= 0) {
-            bmiLabel = getString(R.string.app_name);
-        } else if (Float.compare(bmi, 15f) > 0  &&  Float.compare(bmi, 16f) <= 0) {
-            bmiLabel = getString(R.string.app_name);
-        } else if (Float.compare(bmi, 16f) > 0  &&  Float.compare(bmi, 18.5f) <= 0) {
-            bmiLabel = getString(R.string.app_name);
-        } else if (Float.compare(bmi, 18.5f) > 0  &&  Float.compare(bmi, 25f) <= 0) {
-            bmiLabel = getString(R.string.app_name);
-        } else if (Float.compare(bmi, 25f) > 0  &&  Float.compare(bmi, 30f) <= 0) {
-            bmiLabel = getString(R.string.app_name);
-        } else if (Float.compare(bmi, 30f) > 0  &&  Float.compare(bmi, 35f) <= 0) {
-            bmiLabel = getString(R.string.app_name);
-        } else if (Float.compare(bmi, 35f) > 0  &&  Float.compare(bmi, 40f) <= 0) {
-            bmiLabel = getString(R.string.app_name);
-        } else {
-            bmiLabel = getString(R.string.app_name);
-        }
 
-        bmiLabel = bmi + "\n\n" + bmiLabel;
-        result.setText(bmiLabel);
+    public void initView() {
+        etWeight = findViewById(R.id.weight);
+        etHeight = findViewById(R.id.height);
+        tvResult = findViewById(R.id.result);
+        btnCalculate = findViewById(R.id.calc);
+    }
+
+    public void showResult() {
+        double result = Integer.parseInt(etWeight.getText().toString()) / (Double.parseDouble(etHeight.getText().toString()) * Double.parseDouble(etHeight.getText().toString()));
+
+        DecimalFormat output = new DecimalFormat("#.00");
+
+        if (result < 18.5) tvResult.setText("BMI = " + output.format(result) + "\n Underweight");
+        else if (result >= 18.5 && result < 25)
+            tvResult.setText("BMI = " + output.format(result) + "\n Normal Weight");
+        else tvResult.setText("BMI = " + output.format(result) + "\n Overweight");
+    }
+
+    public void showError() {
+        Toast.makeText(this, "Enter correct Weight and Height \n and Try Again", Toast.LENGTH_LONG).show();
+    }
+
+    public boolean isValid() {
+        if (etHeight.getText().toString().length() == 0 || etWeight.getText().toString().length() == 0 || Double.parseDouble(etHeight.getText().toString()) == 0 || Integer.parseInt(etWeight.getText().toString()) == 0)
+            return false;
+        else
+            return true;
     }
 }
-
-
-
-
 
